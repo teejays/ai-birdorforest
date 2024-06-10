7f692fdffdfd10e38a879aafb85d12f2f2d6c6f3
Talha Ansari <github@teejay.me>
2024-06-10 18:36:24 -0400

    Python server working

--- webservice/main.py
+++ webservice/main.py
@@ -1,12 +1,25 @@
+# Load a fastai model from file
+from fastai.vision.all import *
 
-from fastapi import FastAPI
+learn = load_learner("trained_model.pkl")
+
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/echo/{id}")
+def read_item(id: str = None, q: str = None):
+    return {"id": id, "q": q}
+
+
+@app.post("/classify/")
+def classify_image(image: UploadFile = File(...)):
+    img = PILImage.create(image.file)
+    pred, pred_idx, probs = learn.predict(img)
+    return {"prediction": pred, "probability": probs[pred_idx].item()}
