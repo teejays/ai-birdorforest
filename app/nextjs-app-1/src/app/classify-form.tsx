01780273f7491b6e0490813ae2475f11d6be16ec
Talha Ansari <github@teejay.me>
2024-06-12 17:18:25 -0400

    App: use SERVER_HOST env variable

--- app/nextjs-app-1/src/app/classify-form.tsx
+++ app/nextjs-app-1/src/app/classify-form.tsx
@@ -6,11 +6,14 @@ import { useState } from 'react'
 export const ClassifyForm = (props: {}) => {
     const [response, setResponse] = useState<JSON | null>(null)
 
+    // Read the env variable SERVER_HOST in this react component
+    const serverHost = process.env.SERVER_HOST || '127.0.0.1:8000'
+
     const onFileSubmit = (file: File) => {
         // Send the selected image file to server in a post request
         const formData = new FormData()
         formData.append('image', file)
-        fetch('http://127.0.0.1:8000/classify', {
+        fetch(`http://${serverHost}/classify`, {
             method: 'POST',
             body: formData,
         })
