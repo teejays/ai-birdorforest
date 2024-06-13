d4a3647c77e73edf5d90a8c06a8779abc1e7c1fa
Talha Ansari <github@teejay.me>
2024-06-12 22:32:29 -0400

    Webservice: Remove unused ping scripts

--- webservice/scripts/ping_webserver.py
+++
@@ -1,6 +0,0 @@
-# Python script to ping the webserver at https://ai-birdorforest.onrender.com/echo/foo
-
-import requests
-
-response = requests.get("https://ai-birdorforest.onrender.com/echo/foo")
-print(response.json())
--- webservice/scripts/ping_webserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Description: Script to ping the webserver to keep the server alive
-# Usage: bash cron_ping_webserver.sh
-
-curl https://ai-birdorforest.onrender.com/echo/foo
